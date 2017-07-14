87946c7213f2db20f4576edb10ff6c17e04badf8
Tyler Ferrara <tylerferrara123@gmail.com>
2017-07-13 22:20:15 -0400

    Cleanup Header and Icon Components

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-import Icon from './Icon';
-
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dropDownIsVisible: false }
+  }
+  toggle() {
+    this.setState({ dropDownIsVisible: !this.state.dropDownIsVisible })
+  }
+  showDropDown() {
+    return this.state.dropDownIsVisible ? <DropDown /> : null;
+  }
   render() {
     return (
       <div className="Header">
-        <div className="headerContainer">
-          <Icon className="Icon"/>
+        <div className="contained">
+          <Icon onIconClick={() => this.toggle()}/>
           <div className="location">
-            <h1 className="page">Home</h1>
+            <h1 className="locationTitle">Home</h1>
           </div>
         </div>
+        {this.showDropDown()}
       </div>
     );
   }
+}
+
+const Icon = ({onIconClick}) => {
+  return (
+    <div className="Icon" onClick={onIconClick}>
+      <div id="top" className="bar"></div>
+      <div id="middle" className="bar"></div>
+      <div id="bottom" className="bar"></div>
+    </div>
+  )
+}
+
+const DropDown = () => {
+  return (
+    <div className="DropDown">
+      <div className="contained">
+        <a href="/">Home</a>
+        <hr/>
+        <a href="/">About</a>
+        <hr/>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
