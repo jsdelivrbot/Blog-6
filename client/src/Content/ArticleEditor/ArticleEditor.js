763dcc5fcb753f8a136c9790e087f54f5227b9e6
Tyler Ferrara <tylerferrara123@gmail.com>
2017-08-01 22:19:01 -0400

    Custom Styles for Editor

--- client/src/Content/ArticleEditor/ArticleEditor.js
+++ client/src/Content/ArticleEditor/ArticleEditor.js
@@ -3,30 +3,49 @@ import 'draft-js/dist/Draft.css';
 import './ArticleEditor.css';
 import './ArticleEditor-M.css';
 import EditBar from './EditBar';
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+
+const styleMap = {
+  'STRIKETHROUGH': {
+    textDecoration: 'line-through',
+  },
+};
 
 class ArticleEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
   myBlockStyleFn(contentBlock) {
     return 'editorClass'
   }
+  editBarClick = (style) => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, style));
+  }
   render() {
     return (
       <div className="ArticleEditor">
+        <EditBar onClick={(style) => this.editBarClick(style)} />
         <div className="articleBody container-s container-m">
           <Editor
+          customStyleMap={styleMap}
           editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
           placeholder="Tell a story..."
           spellCheck={true}
           blockStyleFn={this.myBlockStyleFn}/>
         </div>
-
-        <EditBar />
       </div>
     )
   }
--- client/src/Content/ArticleEditor/EditBar.js
+++ client/src/Content/ArticleEditor/EditBar.js
@@ -5,10 +5,11 @@ class EditBar extends Component {
   render() {
     return (
       <div className="EditBar container-s container-m">
-        <button>B</button>
-        <button>I</button>
-        <button>center</button>
-        <button>left-align</button>
+        <button onClick={() => this.props.onClick('BOLD')}><b>B</b></button>
+        <button onClick={() => this.props.onClick('ITALIC')}><i>I</i></button>
+        <button onClick={() => this.props.onClick('UNDERLINE')}>__</button>
+        <button onClick={() => this.props.onClick('CODE')}>&lt;/&gt;</button>
+        <button onClick={() => this.props.onClick('STRIKETHROUGH')}>line-through</button>
       </div>
     )
   }
